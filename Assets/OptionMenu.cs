864fa2bb7aa452458dd6cd236b57da8a18202c02
Chandra-Mauli-Sharma <chandramaulisharma26@gmail.com>
2021-06-12 10:49:41 +0530

    Fps controller pause menu bug fixed

--- Assets/OptionMenu.cs
+++ Assets/OptionMenu.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
 using UnityEngine.UI;
 
-
+namespace UnityStandardAssets.Characters.FirstPerson
+{
 public class OptionMenu : MonoBehaviour
 {
 
     public GameObject option;
+    public GameObject fps;
     public static bool state=false;
     // Update is called once per frame
     void Update()
@@ -32,11 +36,16 @@ public class OptionMenu : MonoBehaviour
         option.SetActive(true);
         state=true;
         Time.timeScale=0f;
+        fps.GetComponent<FirstPersonController>().enabled=false;
+        fps.GetComponent<CameraShake>().enabled=false;
     }
     public void Back()
     {
         option.SetActive(false);
         state=false;
         Time.timeScale=1f;
+fps.GetComponent<FirstPersonController>().enabled=true;
+        fps.GetComponent<CameraShake>().enabled=true;
     }
+}
 }
