8c81a5132fbb21f34b241a280675112d48025f30
Chandra-Mauli-Sharma <chandramaulisharma26@gmail.com>
2021-06-12 20:39:12 +0530

    Timer Added

--- Assets/Mlock.cs
+++ Assets/Mlock.cs
@@ -7,13 +7,13 @@ public class Mlock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
