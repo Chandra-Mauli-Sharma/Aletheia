7119e42d6c872b4c5dbe6ffe8e120226e61f7206
Chandra-Mauli-Sharma <chandramaulisharma26@gmail.com>
2021-06-14 11:50:48 +0530

    Load Scene

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    public GameObject gameObject;
+    public Slider slider;
+    public GameObject bgm;
     public void PlayGame ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadAsynchronously ());
+
     }
     public void QuitGame ()
     {
         Debug.Log("QUIT!");
         Application.Quit();
     }
+    IEnumerator LoadAsynchronously ()
+    {
+        gameObject.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(1);
+       
+        
+        while(!operation.isDone)
+        {
+            float pro=Mathf.Clamp01(operation.progress/.9f);
+            slider.value=pro;
+            yield return null;
+        }
+        if(operation.isDone)
+        {
+            bgm.SetActive(false);
+        }
+    }
+
 }
